44f892ccff8053fb96742bb9c99eba38e3f7b4c8
Dan Ackerman <ardwin@hotmail.com>
2024-07-21 09:42:16 -0400

    systemd service initial

--- os_tweaks/setup_build_node.sh
+++ os_tweaks/setup_build_node.sh
@@ -1,7 +1,35 @@
 #! /bin/sh
 
-sudo cp boot/firmware/config.txt /boot/firmware/config.txt
-sudo cp etc/dphys-swapfile /etc/dphys-swapfile
+# config
+INSTALLDIR="/var/local/office-data-collector"
+
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Starting Now"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+sudo -i
+pushd
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Installing PreReqs"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+
+cp boot/firmware/config.txt /boot/firmware/config.txt
+cp etc/dphys-swapfile /etc/dphys-swapfile
+apt install python3-venv i2c-tools raspi-gpio python3-full
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Installing App"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+mkdir $INSTALLDIR
+python3 -m venv $INSTALLDIR
+cp ../src/app/* $INSTALLDIR
+/bin/sh $INSTALLDIR/bin/activate
+pip3 install -r $INSTALLDIR/requirements.txt
+
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "Rebooting Now"
